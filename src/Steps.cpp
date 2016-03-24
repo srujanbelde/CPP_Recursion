8a917eb3ef89913cc95bab92e022209744758b76
srujanbelde <srujanbelde@missionrnd.com>
2016-03-24 22:20:46 +0530

    Submit Commit

--- src/Steps.cpp
+++ src/Steps.cpp
@@ -26,5 +26,12 @@
 
 int get_steps(int s)
 {
-	return 0;
+	if (s == 0)
+		return 0;
+	else if (s == 1)
+		return 1;
+	else if (s == 2)
+		return 2;
+	else
+		return(get_steps(s - 1) + get_steps(s - 2));
 }
